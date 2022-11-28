ef1478b6ac0ecaa4a542d39a3d8a4f484fd5529c
Nehl-hub <68614152+Nehl-hub@users.noreply.github.com>
2022-11-28 20:58:08 +0100

    Implemented First Benchmark for generate init solution

---
+++ QAPBenchmark/ScatterSearchBenchmarks/GenerateInitialPopulationBenchmarks.cs
@@ -0,0 +1,32 @@
+﻿using BenchmarkDotNet.Attributes;
+using Domain.Models;
+using QAPAlgorithms.ScatterSearch;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QAPBenchmark.ScatterSearchBenchmarks
+{
+    [RPlotExporter]
+    public class GenerateInitialPopulationBenchmarks
+    {
+        private int solutionSize;
+        private ScatterSearchStart scatterSearch;
+
+        [GlobalSetup] 
+        public void Setup() 
+        {
+            solutionSize = 10;
+            var newInstance = new Instance(10, null, null);
+            scatterSearch = new ScatterSearchStart(newInstance);
+        }
+
+        [Benchmark]
+        public void GenerateInitialPopulation()
+        {
+            scatterSearch.GenerateInitialPopulation();
+        }
+    }
+}
