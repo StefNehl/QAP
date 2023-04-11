056a7afd49f4444fdb650c88056b485702197e5e
Nehl-hub <68614152+Nehl-hub@users.noreply.github.com>
2023-04-11 14:10:34 +0200

    Rework of Test method

--- QAP/Program.cs
+++ QAP/Program.cs
@@ -1,6 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-using Domain.Models;
+﻿using Domain.Models;
 using QAP;
 using QAPAlgorithms.ScatterSearch;
 using QAPAlgorithms.ScatterSearch.CombinationMethods;
@@ -10,90 +8,64 @@ using QAPAlgorithms.ScatterSearch.ImprovementMethods;
 
 var qapReader = QAPInstanceReader.QAPInstanceReader.GetInstance();
 
-
-
-//var folderName = "QAPLIB";
-var folderName = "QAPLIBNoOptimum";
-var chr12a = "chr12a.dat";
-var chr25a = "chr25a.dat";
-var tho150 = "tho150.dat";
-var sko42 = "sko42.dat";
-
-
-var filesInFolder = new List<string>() 
+var filesWithKnownOptimum = new List<Tuple<string, string>>
 {
-    sko42,
-    //chr12a,
-    //chr25a,
-    //tho150,
+    new ("QAPLIB","chr12a.dat"),
+    new ("QAPLIB","chr15b.dat"),
+    new ("QAPLIB","chr25a.dat"),
+    new ("QAPLIB","esc16b.dat"),
+    new ("QAPLIB","esc32c.dat"),
+    new ("QAPLIB","esc128.dat"),
+    new ("QAPLIB","nug24.dat"),
+    new ("QAPLIB","nug30.dat"),
+    new ("QAPLIB","sko64.dat"),
+    new ("QAPLIB","tai256c.dat"),
 };
 
-var runtimeInSeconds = 1;
-int refSetSize = 10;
-//17 P_25 P is generally set at max(lOO, 5*refSetSize)
-int populationSetSize = 5 * refSetSize;
 
-//filesInFolder = qapReader.GetFilesInFolder(folderName);
+//new ("QAPLIBNoOptimum", "sko42.dat")
+
+const int runtimeInSeconds = 60;
+
+//17 P_25 P is generally set at max(lOO, 5*refSetSize)
+const int refSetSize = 10;
+const int populationSetSize = 5 * refSetSize;
 
 var testResults = new List<TestResult>();
 var cancellationTokenSource = new CancellationTokenSource();
 
-for(int i = 0; i < filesInFolder.Count; i++)
+for(int i = 0; i < filesWithKnownOptimum.Count; i++)
 {
-    var instance = await qapReader.ReadFileAsync(folderName, filesInFolder[i]);
+    var instance = await qapReader.ReadFileAsync(filesWithKnownOptimum[i].Item1, filesWithKnownOptimum[i].Item2);
+    var testSettings = GetTestSettings(instance, refSetSize, populationSetSize);
+    Console.WriteLine("Sync");
+    var testResult = TestInstance.StartTest(testSettings);
+    Console.WriteLine(testResult.ToStringForConsole());
+    testResults.Add(testResult);
+    Console.WriteLine();
 
-    // Console.WriteLine("Improved First improvement without parallel");
-    // var testResult = GetInstanceWithImprovedFirstImprovement(instance, refSetSize, populationSetSize, runtimeInSeconds);
-    // Console.WriteLine(testResult.ToStringForConsole());
-    // testResults.Add(testResult);
-    // Console.WriteLine();
-
-    Console.WriteLine("Improved Best improvement with parallel");
-    var testResult = await GetInstanceWithImprovedBestImprovement(instance, refSetSize, populationSetSize, runtimeInSeconds);
+    Console.WriteLine("Async");
+    testResult = await TestInstance.StartTestAsync(testSettings,
+        cancellationTokenSource.Token);
     Console.WriteLine(testResult.ToStringForConsole());
     testResults.Add(testResult);
     Console.WriteLine();
     
-    Console.WriteLine($"{i + 1} of {filesInFolder.Count} calculated");
+    Console.WriteLine($"{i + 1} of {filesWithKnownOptimum.Count} calculated");
 }
 
 Console.WriteLine(testResults.First().ToStringColumnNames());
-for(int i = 0; i < testResults.Count; i++)
-{
-    Console.WriteLine(testResults[i].ToString());
-}
+foreach (var t in testResults)
+    Console.WriteLine(t.ToString());
 
 await CSVExport.ExportToCSV(testResults, @"C:\Master_Results", DateTime.Now.ToString("hh-mm-ss_dd-mm-yyyy"));
 
-
-TestResult GetInstanceWithImprovedFirstImprovement(QAPInstance instance, int referenceSetSize, int populationSize, int runTimeInSeconds)      
+TestSettings GetTestSettings(QAPInstance instance, int referenceSetSize, int populationSize)
 {
     var improvementMethod = new ImprovedLocalSearchFirstImprovement(instance);
     var combinationMethod = new ExhaustingPairwiseCombination(1, 10, checkIfSolutionsWereAlreadyCombined: true);
     var generationInitPopMethod = new RandomGeneratedPopulationMethod(instance, 42);
     var diversificationMethod = new HashCodeDiversificationMethod(instance);
-
-    var testSettings = new TestSettings(
-        instance, 
-        populationSize, 
-        referenceSetSize, 
-        runtimeInSeconds, 
-        1,
-        SubSetGenerationMethodType.Cycle, 
-        combinationMethod,
-        generationInitPopMethod,
-        improvementMethod,
-        diversificationMethod);
-    
-    return TestInstance.StartTest(testSettings);
-}
-
-async Task<TestResult> GetInstanceWithImprovedBestImprovement(QAPInstance instance, int referenceSetSize, int populationSize, int runTimeInSeconds)      
-{
-    var improvementMethod = new ImprovedLocalSearchBestImprovement(instance);
-    var combinationMethod = new ExhaustingPairwiseCombination(1, 10, checkIfSolutionsWereAlreadyCombined: true);
-    var generationInitPopMethod = new RandomGeneratedPopulationMethod(instance, 42);
-    var diversificationMethod = new HashCodeDiversificationMethod(instance);
     
     var testSettings = new TestSettings(
         instance, 
@@ -106,6 +78,6 @@ async Task<TestResult> GetInstanceWithImprovedBestImprovement(QAPInstance instan
         generationInitPopMethod,
         improvementMethod,
         diversificationMethod);
-    return await TestInstance.StartTestAsync(testSettings,
-        cancellationTokenSource.Token);
+
+    return testSettings;
 }
