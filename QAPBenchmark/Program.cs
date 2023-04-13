d3795408079810a70910d86d9a7165d93c7069f0
Nehl-hub <68614152+Nehl-hub@users.noreply.github.com>
2023-04-13 09:57:42 +0200

    Implemented benchmark for combined scatter search and path relinking

--- QAPBenchmark/Program.cs
+++ QAPBenchmark/Program.cs
@@ -1,16 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-using BenchmarkDotNet.Running;
+﻿using BenchmarkDotNet.Running;
 using QAPBenchmark.ScatterSearchBenchmarks;
 
 //BenchmarkRunner.Run(typeof(GenerateInitialPopulationBenchmarks).Assembly);
 // BenchmarkRunner.Run<InstanceHelpersBenchmarks>();
 //BenchmarkRunner.Run<ImprovementFirstImprovementParallelBenchmarks>();
 // BenchmarkRunner.Run<SubSetGenerationBenchmark>();
+//BenchmarkRunner.Run<PathRelinkingBenchmarks>();
 
-BenchmarkRunner.Run<PathRelinkingBenchmarks>();
+BenchmarkRunner.Run<PathRelinkingSubSetGenerationCombinedBenchmarks>();
 
-// var test = new PathRelinkingBenchmarks();
+// var test = new PathRelinkingSubSetGenerationCombinedBenchmarks();
 // await test.Setup(); 
 // test.PathRelinking();
 // test.PathRelinking_Parallel();
---
+++ QAPBenchmark/ScatterSearchBenchmarks/PathRelinkingSubSetGenerationCombinedBenchmarks.cs
@@ -0,0 +1,69 @@
+﻿using BenchmarkDotNet.Attributes;
+using Domain.Models;
+using QAPAlgorithms.ScatterSearch;
+using QAPAlgorithms.ScatterSearch.CombinationMethods;
+using QAPAlgorithms.ScatterSearch.ImprovementMethods;
+using QAPAlgorithms.ScatterSearch.SolutionGenerationMethods;
+
+namespace QAPBenchmark.ScatterSearchBenchmarks;
+
+/*
+|                 Method | NrOfCalls |       Mean |    Error |   StdDev | Ratio | RatioSD |    Gen0 |    Gen1 |  Allocated | Alloc Ratio |
+|----------------------- |---------- |-----------:|---------:|---------:|------:|--------:|--------:|--------:|-----------:|------------:|
+|          PathRelinking |       100 |   663.0 us | 13.05 us | 15.03 us |  1.00 |    0.00 |  9.7656 |       - |   487.5 KB |        1.00 |
+| PathRelinking_Parallel |       100 |   989.1 us | 17.60 us | 15.61 us |  1.49 |    0.04 | 13.6719 | 11.7188 |  673.66 KB |        1.38 |
+|                        |           |            |          |          |       |         |         |         |            |             |
+|          PathRelinking |       200 | 1,341.6 us | 26.77 us | 31.86 us |  1.00 |    0.00 | 19.5313 |       - |     975 KB |        1.00 |
+| PathRelinking_Parallel |       200 | 2,129.5 us | 40.35 us | 31.50 us |  1.61 |    0.04 | 27.3438 | 23.4375 | 1350.26 KB |        1.38 |
+ */
+
+[MemoryDiagnoser]
+public class PathRelinkingSubSetGenerationCombinedBenchmarks
+{
+    private PathRelinkingSubSetGenerationCombined _pathRelinkingSubSetGenerationCombined;
+    private ParallelPathRelinkingSubSetGenerationCombined _parallelPathRelinkingSubSetGenerationCombined;
+    private List<InstanceSolution> _referenceSet;
+    
+    [Params(100, 200)] 
+    public int NrOfCalls { get; set; } = 10;
+    
+        
+    [GlobalSetup] 
+    public async Task Setup()
+    {
+        var instanceReader = QAPInstanceReader.QAPInstanceReader.GetInstance();
+        var testInstance = await instanceReader.ReadFileAsync("QAPLIB", "chr12a.dat");
+
+        var improvementMethod = new ImprovedLocalSearchFirstImprovement();
+        improvementMethod.InitMethod(testInstance);
+        var combinationMethod = new ExhaustingPairwiseCombination();
+        combinationMethod.InitMethod(testInstance);
+        
+        _pathRelinkingSubSetGenerationCombined = new PathRelinkingSubSetGenerationCombined(1, SubSetGenerationMethodType.Cycle, improvementMethod, combinationMethod);
+        _pathRelinkingSubSetGenerationCombined.InitMethod(testInstance);
+        _parallelPathRelinkingSubSetGenerationCombined = new ParallelPathRelinkingSubSetGenerationCombined(1, SubSetGenerationMethodType.Cycle, improvementMethod, combinationMethod);
+        _parallelPathRelinkingSubSetGenerationCombined.InitMethod(testInstance);
+        
+        var firstPermutation = new [] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 };
+        var firstSolution = new InstanceSolution(testInstance, firstPermutation);
+        
+        var secondPermutation = new[] { 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 };
+        var secondSolution = new InstanceSolution(testInstance, secondPermutation);
+
+        _referenceSet = new List<InstanceSolution>() { firstSolution, secondSolution };
+    }
+
+    [Benchmark (Baseline = true)]
+    public void PathRelinking()
+    {
+        for(int i = 0; i < NrOfCalls; i++)
+            _pathRelinkingSubSetGenerationCombined.GetSolutions(_referenceSet);
+    }
+        
+    [Benchmark]
+    public void PathRelinking_Parallel()
+    {
+        for(int i = 0; i < NrOfCalls; i++)
+            _parallelPathRelinkingSubSetGenerationCombined.GetSolutions(_referenceSet);
+    }
+}
