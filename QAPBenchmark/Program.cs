820a3ce49a6e1a915bc8977a702857c366e755d5
Nehl-hub <68614152+Nehl-hub@users.noreply.github.com>
2023-04-14 13:19:16 +0200

    implemented benchmarks for the init generation population

--- QAPBenchmark/Program.cs
+++ QAPBenchmark/Program.cs
@@ -6,11 +6,8 @@ BenchmarkRunner.Run<GenerateInitialPopulationBenchmarks>();
 // BenchmarkRunner.Run<ImprovementBenchmarks>();
 // BenchmarkRunner.Run<SolutionGenerationBenchmarks>();
 
-// var test = new ImprovementBenchmarks();
+// var test = new GenerateInitialPopulationBenchmarks();
 // test.Setup(); 
-// test.LocalSearchBestImprovement_ImproveSolutions();
-// test.ImprovedLocalSearchBestImprovement_ImproveSolutions();
-// test.LocalSearchBestImprovement_ImproveSolutions_Parallel();
-// test.LocalSearchFirstImprovement_ImproveSolutions();
-// test.ImprovedLocalSearchFirstImprovement_ImproveSolutions();
-// test.LocalSearchFirstImprovement_ImproveSolutions_Parallel();
+// test.StepWisePopulationGeneration();
+// test.RandomGeneratedPopulation();
+// test.ParallelRandomGeneratedPopulation();
--- QAPBenchmark/ScatterSearchBenchmarks/GenerateInitialPopulationBenchmarks.cs
+++ QAPBenchmark/ScatterSearchBenchmarks/GenerateInitialPopulationBenchmarks.cs
@@ -11,26 +11,35 @@ using System.Threading.Tasks;
 using QAPAlgorithms.ScatterSearch.InitGenerationMethods;
 
 /*
-|                       Method | SolutionName | NrOfSolutions |          Mean |       Error |      StdDev |        Median |   Gen0 | Allocated |
-|----------------------------- |------------- |-------------- |--------------:|------------:|------------:|--------------:|-------:|----------:|
-| StepWisePopulationGeneration |   chr12a.dat |            10 |      4.874 us |   0.0974 us |   0.1365 us |      4.840 us | 0.0153 |    1016 B |
-|    RandomGeneratedPopulation |   chr12a.dat |            10 |      7.363 us |   0.1299 us |   0.1215 us |      7.419 us | 0.0153 |    1016 B |
-| StepWisePopulationGeneration |   chr12a.dat |           100 |     48.247 us |   0.9508 us |   1.1319 us |     47.491 us | 0.1831 |    9656 B |
-|    RandomGeneratedPopulation |   chr12a.dat |           100 |     73.880 us |   1.4565 us |   1.4305 us |     74.652 us | 0.1221 |    9656 B |
-| StepWisePopulationGeneration |   chr12a.dat |           200 |     97.961 us |   1.9206 us |   1.8863 us |     97.865 us | 0.3662 |   19256 B |
-|    RandomGeneratedPopulation |   chr12a.dat |           200 |    150.276 us |   2.8657 us |   3.7262 us |    149.364 us | 0.2441 |   19256 B |
-| StepWisePopulationGeneration |   chr25a.dat |            10 |     14.548 us |   0.2871 us |   0.3191 us |     14.574 us | 0.0305 |    1576 B |
-|    RandomGeneratedPopulation |   chr25a.dat |            10 |     20.315 us |   0.3975 us |   0.4082 us |     20.432 us | 0.0305 |    1576 B |
-| StepWisePopulationGeneration |   chr25a.dat |           100 |    147.654 us |   2.8489 us |   3.3914 us |    148.920 us | 0.2441 |   15256 B |
-|    RandomGeneratedPopulation |   chr25a.dat |           100 |    203.134 us |   3.3428 us |   2.9633 us |    204.264 us | 0.2441 |   15256 B |
-| StepWisePopulationGeneration |   chr25a.dat |           200 |    292.767 us |   5.7077 us |   6.7946 us |    294.985 us | 0.4883 |   30456 B |
-|    RandomGeneratedPopulation |   chr25a.dat |           200 |    406.864 us |   7.8363 us |   8.3848 us |    408.455 us | 0.4883 |   30456 B |
-| StepWisePopulationGeneration |  tai256c.dat |            10 |  1,024.597 us |   3.4116 us |   2.6636 us |  1,023.710 us |      - |   10778 B |
-|    RandomGeneratedPopulation |  tai256c.dat |            10 |  1,128.514 us |  22.1456 us |  25.5029 us |  1,134.361 us |      - |   10778 B |
-| StepWisePopulationGeneration |  tai256c.dat |           100 | 10,402.005 us | 130.7088 us | 102.0488 us | 10,443.596 us |      - |  107275 B |
-|    RandomGeneratedPopulation |  tai256c.dat |           100 | 11,398.299 us | 218.6564 us | 224.5440 us | 11,491.681 us |      - |  107274 B |
-| StepWisePopulationGeneration |  tai256c.dat |           200 | 20,904.533 us | 369.8356 us | 345.9445 us | 21,082.506 us |      - |  214491 B |
-|    RandomGeneratedPopulation |  tai256c.dat |           200 | 22,909.002 us | 445.1163 us | 476.2695 us | 23,022.966 us |      - |  214491 B |
+|                            Method | SolutionName | NrOfSolutions |          Mean |       Error |      StdDev |        Median |    Gen0 |   Gen1 | Allocated |
+|---------------------------------- |------------- |-------------- |--------------:|------------:|------------:|--------------:|--------:|-------:|----------:|
+|      StepWisePopulationGeneration |   chr12a.dat |            10 |      4.843 us |   0.0945 us |   0.0928 us |      4.811 us |  0.0153 |      - |    1016 B |
+|         RandomGeneratedPopulation |   chr12a.dat |            10 |      7.514 us |   0.1410 us |   0.1319 us |      7.553 us |  0.0153 |      - |    1016 B |
+| ParallelRandomGeneratedPopulation |   chr12a.dat |            10 |      8.794 us |   0.1267 us |   0.1185 us |      8.758 us |  0.1068 |      - |    5809 B |
+|      StepWisePopulationGeneration |   chr12a.dat |           100 |     49.469 us |   0.9659 us |   0.9035 us |     49.377 us |  0.1831 |      - |    9656 B |
+|         RandomGeneratedPopulation |   chr12a.dat |           100 |     74.060 us |   1.2752 us |   1.7023 us |     73.683 us |  0.1221 |      - |    9656 B |
+| ParallelRandomGeneratedPopulation |   chr12a.dat |           100 |     76.739 us |   0.9537 us |   0.8921 us |     77.163 us |  0.9766 |      - |   53462 B |
+|      StepWisePopulationGeneration |   chr12a.dat |           200 |     98.217 us |   1.9158 us |   2.1294 us |     98.070 us |  0.3662 |      - |   19256 B |
+|         RandomGeneratedPopulation |   chr12a.dat |           200 |    147.632 us |   2.9410 us |   3.5011 us |    147.416 us |  0.2441 |      - |   19256 B |
+| ParallelRandomGeneratedPopulation |   chr12a.dat |           200 |    148.614 us |   1.9131 us |   1.7895 us |    148.192 us |  1.9531 |      - |  106539 B |
+|      StepWisePopulationGeneration |   chr25a.dat |            10 |     14.651 us |   0.2783 us |   0.2603 us |     14.728 us |  0.0305 |      - |    1576 B |
+|         RandomGeneratedPopulation |   chr25a.dat |            10 |     20.169 us |   0.3886 us |   0.4915 us |     20.090 us |  0.0305 |      - |    1576 B |
+| ParallelRandomGeneratedPopulation |   chr25a.dat |            10 |     20.047 us |   0.3176 us |   0.2971 us |     20.045 us |  0.1526 |      - |    7951 B |
+|      StepWisePopulationGeneration |   chr25a.dat |           100 |    146.729 us |   2.8841 us |   3.7502 us |    147.126 us |  0.2441 |      - |   15256 B |
+|         RandomGeneratedPopulation |   chr25a.dat |           100 |    206.422 us |   4.0633 us |   6.6761 us |    205.858 us |  0.2441 |      - |   15256 B |
+| ParallelRandomGeneratedPopulation |   chr25a.dat |           100 |    133.706 us |   0.1667 us |   0.1478 us |    133.663 us |  1.4648 |      - |   74297 B |
+|      StepWisePopulationGeneration |   chr25a.dat |           200 |    290.482 us |   2.8887 us |   2.5607 us |    290.916 us |  0.4883 |      - |   30456 B |
+|         RandomGeneratedPopulation |   chr25a.dat |           200 |    397.811 us |   4.5838 us |   4.0634 us |    396.668 us |  0.4883 |      - |   30456 B |
+| ParallelRandomGeneratedPopulation |   chr25a.dat |           200 |    250.008 us |   0.6600 us |   0.6174 us |    249.835 us |  2.9297 |      - |  148140 B |
+|      StepWisePopulationGeneration |  tai256c.dat |            10 |  1,032.983 us |  12.2610 us |  10.8691 us |  1,029.769 us |       - |      - |   10778 B |
+|         RandomGeneratedPopulation |  tai256c.dat |            10 |  1,120.071 us |   9.0009 us |   8.4194 us |  1,114.568 us |       - |      - |   10778 B |
+| ParallelRandomGeneratedPopulation |  tai256c.dat |            10 |    283.161 us |   0.3709 us |   0.3288 us |    283.108 us |  0.4883 |      - |   35793 B |
+|      StepWisePopulationGeneration |  tai256c.dat |           100 | 10,149.696 us |  47.3119 us |  44.2556 us | 10,118.733 us |       - |      - |  107274 B |
+|         RandomGeneratedPopulation |  tai256c.dat |           100 | 11,163.472 us | 102.3644 us |  95.7518 us | 11,087.425 us |       - |      - |  107274 B |
+| ParallelRandomGeneratedPopulation |  tai256c.dat |           100 |  1,717.496 us |   1.9562 us |   1.5272 us |  1,717.831 us |  5.8594 |      - |  352706 B |
+|      StepWisePopulationGeneration |  tai256c.dat |           200 | 20,376.865 us | 120.7136 us | 112.9156 us | 20,457.331 us |       - |      - |  214491 B |
+|         RandomGeneratedPopulation |  tai256c.dat |           200 | 22,199.417 us | 212.4541 us | 198.7297 us | 22,084.953 us |       - |      - |  214493 B |
+| ParallelRandomGeneratedPopulation |  tai256c.dat |           200 |  3,263.804 us |   9.5618 us |   7.9845 us |  3,261.271 us | 11.7188 | 3.9063 |  704948 B |
  */
 
 namespace QAPBenchmark.ScatterSearchBenchmarks
@@ -65,6 +74,7 @@ namespace QAPBenchmark.ScatterSearchBenchmarks
             _randomGenerationPopulation.InitMethod(instance);
 
             _parallelRandomGeneratedPopulation = new ParallelRandomGeneratedPopulation(42);
+            _parallelRandomGeneratedPopulation.InitMethod(instance);
             
         }
 
