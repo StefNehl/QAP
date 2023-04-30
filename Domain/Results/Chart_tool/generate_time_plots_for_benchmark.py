1aa94db9cd6f8688194d3bbfee0a462d514c2d1c
Nehl-hub <68614152+Nehl-hub@users.noreply.github.com>
2023-04-30 13:25:38 +0200

    Added benchmarks for Improvement Benchmarks

--- Domain/Results/Chart_tool/generate_time_plots_for_benchmark.py
+++ Domain/Results/Chart_tool/generate_time_plots_for_benchmark.py
@@ -2,7 +2,12 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-def generate_time_plot(file_path: str, plot_name: str):
+def generate_time_plot(file_path: str,
+                       plot_name: str,
+                       y_axis_name: str,
+                       y_axis_label: str,
+                       x_axis_name: str,
+                       x_axis_label: str):
 
     data = pd.read_csv(file_path)
 
@@ -15,19 +20,24 @@ def generate_time_plot(file_path: str, plot_name: str):
         second_groups = first_group[1].groupby("Method")
 
         for second_group in second_groups:
-            y = second_group[1]["Mean[us]"]
-            x = second_group[1]["NrOfSolutions"]
+            y = second_group[1][y_axis_name]
+            x = second_group[1][x_axis_name]
             plt.plot(x, y, linewidth=1, label=second_group[0])
 
         plt.title(plot_name + " " + first_group[0])
-        plt.xlabel("Nr of solutions")
-        plt.ylabel("Time [us]")
+        plt.xlabel(x_axis_label)
+        plt.ylabel(y_axis_label)
         plt.tight_layout()
         plt.legend()
         plt.show()
 
 
-def generate_memory_plot(file_path: str, plot_name: str):
+def generate_memory_plot(file_path: str,
+                         plot_name: str,
+                         y_axis_name: str,
+                         y_axis_label: str,
+                         x_axis_name: str,
+                         x_axis_label: str):
     data = pd.read_csv(file_path)
     first_groups = data.groupby("SolutionName")
 
@@ -38,13 +48,13 @@ def generate_memory_plot(file_path: str, plot_name: str):
         second_groups = first_group[1].groupby("Method")
 
         for second_group in second_groups:
-            y = second_group[1]["Allocated[B]"]
-            x = second_group[1]["NrOfSolutions"]
+            y = second_group[1][y_axis_name]
+            x = second_group[1][x_axis_name]
             plt.plot(x, y, linewidth=1, label=second_group[0])
 
         plt.title(plot_name + " " + first_group[0])
-        plt.xlabel("Nr of solutions")
-        plt.ylabel("Allocation [B]")
+        plt.xlabel(x_axis_label)
+        plt.ylabel(y_axis_label)
         plt.tight_layout()
         plt.legend()
         plt.show()
