11604e81ea235b0460b42900eb736c48ddb47938
Nehl-hub <68614152+Nehl-hub@users.noreply.github.com>
2023-05-01 10:49:49 +0200

    Implemented automatic plot generation

--- Domain/Results/Chart_tool/generate_time_plots_for_benchmark.py
+++ Domain/Results/Chart_tool/generate_time_plots_for_benchmark.py
@@ -1,43 +1,14 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+from pandas.core.groupby import DataFrameGroupBy
 
 
-def generate_time_plot(file_path: str,
-                       plot_name: str,
-                       y_axis_name: str,
-                       y_axis_label: str,
-                       x_axis_name: str,
-                       x_axis_label: str):
-
-    data = pd.read_csv(file_path)
-
-    first_groups = data.groupby("SolutionName")
-
-    for g in first_groups:
-        print(g)
-
-    for first_group in first_groups:
-        second_groups = first_group[1].groupby("Method")
-
-        for second_group in second_groups:
-            y = second_group[1][y_axis_name]
-            x = second_group[1][x_axis_name]
-            plt.plot(x, y, linewidth=1, label=second_group[0])
-
-        plt.title(plot_name + " " + first_group[0])
-        plt.xlabel(x_axis_label)
-        plt.ylabel(y_axis_label)
-        plt.tight_layout()
-        plt.legend()
-        plt.show()
-
-
-def generate_memory_plot(file_path: str,
-                         plot_name: str,
-                         y_axis_name: str,
-                         y_axis_label: str,
-                         x_axis_name: str,
-                         x_axis_label: str):
+def generate_plot(file_path: str,
+                  plot_name: str,
+                  y_axis_name: str,
+                  y_axis_label: str,
+                  x_axis_name: str,
+                  x_axis_label: str):
     data = pd.read_csv(file_path)
     first_groups = data.groupby("SolutionName")
 
@@ -47,14 +18,33 @@ def generate_memory_plot(file_path: str,
     for first_group in first_groups:
         second_groups = first_group[1].groupby("Method")
 
-        for second_group in second_groups:
-            y = second_group[1][y_axis_name]
-            x = second_group[1][x_axis_name]
-            plt.plot(x, y, linewidth=1, label=second_group[0])
+        show_plot_for_group(first_group,
+                            second_groups,
+                            plot_name,
+                            y_axis_name,
+                            y_axis_label,
+                            x_axis_name,
+                            x_axis_label)
 
-        plt.title(plot_name + " " + first_group[0])
-        plt.xlabel(x_axis_label)
-        plt.ylabel(y_axis_label)
-        plt.tight_layout()
-        plt.legend()
-        plt.show()
+
+def show_plot_for_group(first_group,
+                        second_groups: DataFrameGroupBy,
+                        plot_name: str,
+                        y_axis_name: str,
+                        y_axis_label: str,
+                        x_axis_name: str,
+                        x_axis_label: str):
+    for second_group in second_groups:
+        y = second_group[1][y_axis_name]
+        x = second_group[1][x_axis_name]
+        plt.plot(x, y, linewidth=1, label=second_group[0])
+
+    title = plot_name + " " + first_group[0]
+    plt.title(title)
+    plt.xlabel(x_axis_label)
+    plt.ylabel(y_axis_label)
+    plt.tight_layout()
+    plt.legend()
+    fig = plt.gcf()
+    plt.show()
+    fig.savefig("..\\Benchmarks\\Plots\\" + title + ".png")
--- Domain/Results/Chart_tool/main.py
+++ Domain/Results/Chart_tool/main.py
@@ -1,3 +1,29 @@
+from generate_time_plots_for_benchmark import *
 
+generate_plot('..\\Benchmarks\\GenerateInitialPopulation_Time_Results.CSV',
+              "Initial Generation Methods",
+              "Mean[us]",
+              "Time[us]",
+              "NrOfSolutions",
+              "Nr of Solutions")
 
+generate_plot("..\\Benchmarks\\GenerateInitialPopulation_Memory_Results.CSV",
+              "Initial Generation Methods",
+              "Allocated[B]",
+              "Allocated[B]",
+              "NrOfSolutions",
+              "Nr of Solutions")
 
+generate_plot("..\\Benchmarks\\ImprovementBenchmarks_Time_Results.csv",
+              "Improvement Methods",
+              "Mean[ns]",
+              "Time[ns]",
+              "NrOfSolutions",
+              "Nr of Solutions")
+
+generate_plot("..\\Benchmarks\\ImprovementBenchmarks_Memory_Results.csv",
+              "Improvement Methods",
+              "Allocated[B]",
+              "Allocated[B]",
+              "NrOfSolutions",
+              "Nr of Solutions")
