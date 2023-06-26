c017e8a54e59b27b9bbf2ec8f5b5133857b34d0d
Nehl-hub <68614152+Nehl-hub@users.noreply.github.com>
2023-06-26 21:16:32 +0200

    Fixed stack overflow

--- QAPAlgorithms/ScatterSearch/ImprovementMethods/ParallelImprovedLocalSearchFirstImprovement.cs
+++ QAPAlgorithms/ScatterSearch/ImprovementMethods/ParallelImprovedLocalSearchFirstImprovement.cs
@@ -9,7 +9,7 @@ public class ParallelImprovedLocalSearchFirstImprovement : ImprovedLocalSearchFi
     {
         if (instanceSolutions.Count <= 5)
         {
-            ImproveSolutions(instanceSolutions);
+            base.ImproveSolutions(instanceSolutions);
             return;
         }
 
