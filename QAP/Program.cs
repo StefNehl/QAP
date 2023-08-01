0ddc3bc5071585940e4a2e9bd91082430c42fe0a
Stefan Nehl <s.nehl@fesios.com>
2023-08-01 22:16:56 +0200

    adjusted unknown optimum instances

--- QAP/Program.cs
+++ QAP/Program.cs
@@ -19,27 +19,39 @@ var filesWithKnownOptimum = new List<TestFiles>
     new ("QAPLIB","tai256c.dat", 44759294),
 };
 
-//new ("QAPLIBNoOptimum", "sko42.dat")
+var filesWithUnknownOptimum = new List<TestFiles>
+{
+    new("QAPLIBNoOptimum", "sko42.dat", 14934),
+    new("QAPLIBNoOptimum", "sko64.dat", 45736),
+    new("QAPLIBNoOptimum", "sko90.dat", 108493),
+    new("QAPLIBNoOptimum", "sko100c.dat", 139402),
+    new("QAPLIBNoOptimum", "tai30a.dat", 1529135),
+    new("QAPLIBNoOptimum", "tai50a.dat", 3854359),
+    new("QAPLIBNoOptimum", "tai100a.dat", 15824355),
+    new("QAPLIBNoOptimum", "tho40.dat", 214218),
+    new("QAPLIBNoOptimum", "tho150.dat", 7620628),
+    new("QAPLIBNoOptimum", "wil100.dat", 263909),
+};
 
-const int runtimeInSeconds = 600;
+const int runtimeInSeconds = 6;
 // const int runtimeInSeconds = 600 * 3;
 //17 P_25 P is generally set at max(lOO, 5*refSetSize)
 int nrOfRepetitions = 1;
 var testResults = new List<TestResult>();
 
-var calculatedRuntime = filesWithKnownOptimum.Count * runtimeInSeconds * nrOfRepetitions * 4; // 4 = nr of tests 
+var calculatedRuntime = filesWithUnknownOptimum.Count * runtimeInSeconds * nrOfRepetitions * 4; // 4 = nr of tests 
 Console.WriteLine("Runtime till: " + DateTime.Now.AddSeconds(calculatedRuntime));
 
-for(int i = 0; i < filesWithKnownOptimum.Count; i++)
+for(int i = 0; i < filesWithUnknownOptimum.Count; i++)
 {
     int refSetSize = 20;
     int populationSetSize = 5 * refSetSize;
-    Console.WriteLine($"Instance: {filesWithKnownOptimum[i].FileName} {i + 1} of {filesWithKnownOptimum.Count}.");
+    Console.WriteLine($"Instance: {filesWithUnknownOptimum[i].FileName} {i + 1} of {filesWithUnknownOptimum.Count}.");
     for (int r = 0; r < nrOfRepetitions; r++)
     {
         Console.WriteLine("Repetition: " + (r + 1) + " of " + nrOfRepetitions);
-        var instance = await qapReader.ReadFileAsync(filesWithKnownOptimum[i].FolderName, 
-            filesWithKnownOptimum[i].FileName);
+        var instance = await qapReader.ReadFileAsync(filesWithUnknownOptimum[i].FolderName, 
+            filesWithUnknownOptimum[i].FileName);
         var testSettingsProvider = new TestSettingsProvider(instance, refSetSize, populationSetSize, runtimeInSeconds);
 
         int nrOfTest = testSettingsProvider.GetTestSettings().Count;
@@ -50,7 +62,7 @@ for(int i = 0; i < filesWithKnownOptimum.Count; i++)
             var testResult = TestInstance.StartTest(
                 testSetting, 
                 false, 
-                filesWithKnownOptimum[i].KnownOptimum, 
+                filesWithUnknownOptimum[i].KnownOptimum, 
                 true);
             Console.WriteLine(testResult.ToString());
             testResults.Add(testResult);
